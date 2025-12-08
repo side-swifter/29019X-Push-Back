6ab519d3496bebf0ac1d83311f5c1e61a8941564
side-swifter <sideswifter2010@gmail.com>
2025-12-08 17:56:45 -0500

    first commit

---
+++ include/subsystems.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "EZ-Template/piston.hpp"
+#include "pros/adi.h"
+#include "pros/motor_group.hpp"
+#pragma once
+
+#include "pros/motors.hpp"
+#include "EZ-Template/drive/drive.hpp"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+inline pros::MotorGroup intake({7,-8,9,-10});  // Negative port will reverse the motor 
+
+inline ez::Piston scraper('H');     
+inline ez::Piston switcher('G');
+inline ez::Piston hood('F');
